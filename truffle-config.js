bcc11662fda50057536793ebdc709ed542f7f85f
uptownpanda <info@uptownpanda.finance>
2020-11-22 00:30:37 +0100

    empty line removal

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,4 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-
 const fs = require('fs');
 const ownerWalletPrivateKey = fs.readFileSync('.owner-wallet-private-key').toString().trim();
 const etherscanApiKey = fs.readFileSync('.etherscan.apikey').toString().trim();
